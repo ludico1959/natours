c0c4a341f4a23a366b823f49f6fca9149d8f5b2c
ludico1959 <ludico1959@gmail.com>
2021-12-21 18:10:25 -0300

    handle post requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// Middleware: function that can modify the incoming request data.
+app.use(express.json());
+
 // JSON.parse() converts JSON into JS.
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -16,6 +19,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
