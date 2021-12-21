89622c75662471e3c109f04d1337ac50f243bb51
ludico1959 <ludico1959@gmail.com>
2021-12-21 17:45:35 -0300

    Handle get requests

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+const fs = require('fs');
 const express = require('express');
-const res = require('express/lib/response');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side! 💚', app: 'Natours' });
-});
+// JSON.parse() converts JSON into JS.
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint!');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
