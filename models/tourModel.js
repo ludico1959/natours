db7ae07c09a95b115472770670e3abbb050b8cf5
Lucas Dias Coelho <ludico1959@gmail.com>
2022-02-06 19:47:49 -0300

    feat(model): create a duration weeks virtual propoertie and show it in the response

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,58 +1,69 @@
 const mongoose = require('mongoose');
 
 // Schema:
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a maximum group size'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, // Remove all empty space in the beginning and in the end of the string.
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String, // Just the name of the image, like a reference.
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], // Set a array which elements are strings.
+    createdAt: {
+      type: Date,
+      default: Date.now(), // In MongoDB, this date function is converted in a normal date format.
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a maximum group size'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true, // Remove all empty space in the beginning and in the end of the string.
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String, // Just the name of the image, like a reference.
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], // Set a array which elements are strings.
-  createdAt: {
-    type: Date,
-    default: Date.now(), // In MongoDB, this date function is converted in a normal date format.
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Virtual Properties:
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 // Model:
