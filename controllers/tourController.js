e7543dbbb784a512b48089c668d9755d7d2f61c0
Lucas Dias Coelho <ludico1959@gmail.com>
2022-02-08 11:31:16 -0300

    feat(error): handle not found uuid's passed in the request for route handlers that use id param

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopTours = (req, res, next) => {
@@ -33,6 +34,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // Same as 'Tour.findOne({ _id: req.params.id })'
 
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -58,6 +63,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true, // Validates the update operation through the model's schema validators.
   });
 
+  if (!modifiedTour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'sucess',
     data: {
@@ -67,7 +76,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const deletedTour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!deletedTour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -3,6 +3,6 @@
 
 module.exports = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch(next);
+    fn(req, res, next).catch((error) => next(error));
   };
 };
