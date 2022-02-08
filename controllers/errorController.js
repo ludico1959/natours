e2a7c59225be8daee507cd3bd8dc48571de19ffa
Lucas Dias Coelho <ludico1959@gmail.com>
2022-02-08 12:15:48 -0300

    feat(error): send different responses for development and production environments

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProd = (error, res) => {
+  /* These if/else blocks are for not showing to the client the operational errors.
+   * It's a good practice because we don't want to leak error details to the clients.
+   */
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    console.error('ERROR ❗❗❗', error);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong 😓',
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(error, res);
+  }
 };
