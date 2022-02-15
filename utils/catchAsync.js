ca812c56c73456d019026a4dc47dcd6c84795633
Lucas Dias Coelho <ludico1959@gmail.com>
2022-02-15 16:41:17 -0300

    refactor(utils): rename parameter from catch async function

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,8 +1,8 @@
 /* eslint-disable arrow-body-style */
-// This is a solution if you want to get rid of try/catchh blocks from the Controller!
 
-module.exports = (fn) => {
+// This is a solution if you want to get rid of try/catch blocks from the Controller!
+module.exports = (routeHandlerFunction) => {
   return (req, res, next) => {
-    fn(req, res, next).catch((error) => next(error));
+    routeHandlerFunction(req, res, next).catch((error) => next(error));
   };
 };
